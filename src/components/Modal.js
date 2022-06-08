20b225ba8183ee5659f41ade3e26af0b8623a075
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-06-08 19:49:49 +0530

    added create without calling api

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,21 +1,39 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Modal(props) {
     const [myTitle, setMyTitle] = useState('');
     const [desc, setDesc] = useState('');
+    
 
-    const handleTitleChange = (e)=>{
+    const handleTitleChange = (e) => {
         setMyTitle(e.target.value);
     }
-    const handleDescChange = (e)=>{
+    const handleDescChange = (e) => {
         setDesc(e.target.value);
     }
 
-    const handleSave = ()=>{
-        let obj={
-            title:myTitle,
-            description:desc
+    const handleSave = async (e) => {
+        e.preventDefault()
+        let obj = {
+            title: myTitle,
+            description: desc,
+            _id: Date.now()
         }
+
+        let res = await fetch('/api/createNote', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: myTitle,
+                description: desc
+            })
+        })
+
+        let data = await res.json();
+        console.log(data)
+
         props.changeData(obj);
     }
 
@@ -35,20 +53,27 @@ export default function Modal(props) {
                             <h5 className="modal-title" id="staticBackdropLabel">New Note</h5>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div className="modal-body">
-                            <div className="mb-3">
-                                <label htmlFor="exampleFormControlInput1" className="form-label">Title</label>
-                                <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Enter the Title" value={myTitle} onChange={handleTitleChange}/>
+
+
+                        <form class="row g-3 needs-validation" novalidate  onSubmit={handleSave}>
+                            <div className="modal-body">
+                                <div className="mb-3">
+                                    <label htmlFor="validationCustom01" className="form-label">Title</label>
+                                    <input type="text" class="form-control" id="validationCustom02" required placeholder="Enter the Title" value={myTitle} onChange={handleTitleChange} />
+                                    <div class="valid-feedback">
+                                        Looks good!
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="validationCustom02" className="form-label">Description</label>
+                                    <textarea className="form-control" id="validationCustom02" required placeholder='Enter the Description' rows="3" value={desc} onChange={handleDescChange}></textarea>
+                                </div>
                             </div>
-                            <div className="mb-3">
-                                <label htmlFor="exampleFormControlTextarea1" className="form-label">Description</label>
-                                <textarea className="form-control" id="exampleFormControlTextarea1" placeholder='Enter the Description' rows="3" value={desc} onChange={handleDescChange}></textarea>
+                            <div className="modal-footer">
+                                <button type="submit" className="btn btn-primary" data-bs-dismiss={`${myTitle.length!==0?'modal':''}`}>Save</button>
+                                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                             </div>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-primary" onClick={handleSave}>Save</button>
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/components/Notes.js
+++ src/components/Notes.js
@@ -4,7 +4,7 @@ import NoteItem from './NoteItem';
 
 export default function Notes() {
   const [data, setData] = useState([]);
-  const [unsavedData, setUnsavedData] = useState([])
+  // const [unsavedData, setUnsavedData] = useState([])
 
   async function fetchData() {
     let response = await fetch('/api/notes');
@@ -12,48 +12,35 @@ export default function Notes() {
     setData(parsedData)
   }
 
-  const changeData =(newData)=>{
-    setUnsavedData(unsavedData.concat(newData))
-    setData(data.concat(unsavedData));
-  }
-
-  const handleUnmount = async ()=>{
-    console.log('unmounting...')
-    if(unsavedData!==[])
-    {
-      console.log('saving...')
-      let response= await fetch('/api/createNote',{
-        method:'POST',
-      headers:{
-        'Content-Type':'application/json'
-      },
-      body:JSON.stringify(unsavedData)
-    }).then(res=>{res.json()});
-  }
+  const changeData = (newData) => {
+    setData(data.concat(newData));
+      // console.log(unsavedData)
   }
 
   useEffect(() => {
+    if(data.length===0)
     fetchData();
+        
+      
+    
+  },[]);
 
-    return handleUnmount
-  }, []);
-  
 
 
   return (
     <>
-      <div className='container' style={{marginTop:"7rem"}}>
+      <div className='container' style={{ marginTop: "7rem" }}>
         <div className='row'>
           {
             data.map((elem) => {
-              return <div className='col-md-4  mb-4' key={elem.id}>
+              return <div className='col-md-4  mb-4' key={elem._id}>
                 <NoteItem title={elem.title} desc={elem.description} date={elem.date} />
               </div>
             })
           }
         </div>
       </div>
-      <Modal changeData={changeData}/>
+      <Modal changeData={changeData} />
     </>
   )
 }
