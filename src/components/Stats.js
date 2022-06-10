0001ac71e004beda7de6561ff883e065065ecd63
Aditya Narayan Jha <adityanarayanjha31@gmail.com>
2022-06-10 07:20:51 +0530

    Spinner in stats and changed proxy

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,9 +1,12 @@
-import React,{useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import { Chart, registerables } from 'chart.js'
+import Spinner from './Spinner';
 
 export default function Stats(props) {
     let myChart = null;
     Chart.register(...registerables)
+
+    const [loading, setLoading] = useState(true);
 
     const labels = [
         'Sunday',
@@ -15,46 +18,48 @@ export default function Stats(props) {
         'Saturday'
     ];
 
-    
+
 
 
     useEffect(() => {
-        let count=0;
-        if( count === 0)
-        {if (myChart !== null) {
-            myChart.destroy();
-        }
-        props.statsData.then((res)=>{
-            
-            const data = {
-                labels: labels,
-                datasets: [{
-                    label: 'Notes Added or Updated',
-                    backgroundColor: 'rgb(255, 99, 132)',
-                    borderColor: 'rgb(255, 99, 132)',
-                    data: res
-                }]
-            };
+        let count = 0;
+        if (count === 0) {
+            if (myChart !== null) {
+                myChart.destroy();
+            }
+            props.statsData.then((res) => {
+                setLoading(false)
+                const data = {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Notes Added or Updated',
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: res
+                    }]
+                };
 
-            
-            const config = {
-                type: 'line',
-                data: data,
-                options: {}
-            };
 
-            // eslint-disable-next-line
-            myChart = new Chart(
-                document.getElementById('myChart'),
-                config
+                const config = {
+                    type: 'line',
+                    data: data,
+                    options: {}
+                };
+
+                // eslint-disable-next-line
+                myChart = new Chart(
+                    document.getElementById('myChart'),
+                    config
                 );
-            })}
-            count++;
-        
+            })
+
+        }
+        count++;
     }, []);
 
     return (
         <div style={{ marginTop: '7rem' }}>
+            {loading && <Spinner />}
             <div>
                 <canvas id={`myChart`} width="270" height="100"></canvas>
             </div>
